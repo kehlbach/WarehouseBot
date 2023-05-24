55f0690bbe1cb8b56164609e244eeca393dab175
Valentin Kelbakh <valentinkelbax@gmail.com>
2023-05-24 23:02:15 +0600

    check permissions before processing menu choice

--- app/handlers/menu.py
+++ app/handlers/menu.py
@@ -43,32 +43,47 @@ async def process_menu_init(callback_query: CallbackQuery, callback_data: dict, 
 async def process_menu_choice(callback_query: CallbackQuery, callback_data: dict, state: FSMContext):
     user_id = callback_query['from']['id']
     master = db.filter(db.PROFILES, user_id=user_id)
+    permissions = tools.permissions(master)
     match callback_data['action']:
         case Menu.PROFILES:
+            if not permissions[PROFILES]:
+                raise PermissionError
             profiles_page = db.get_page(db.PROFILES,callback_data.get('page',1))
             text = 'Выберите пользователя'
             reply_markup = kb.get_profiles(master,profiles_page, callback_data.get('page',1))
         case Menu.ROLES:
+            if not permissions[ROLES]:
+                raise PermissionError
             roles_page = db.get_page(db.ROLES,callback_data.get('page',1))
             text = 'Выберите роль'
             reply_markup = kb.get_roles(master,roles_page, callback_data.get('page',1))
         case Menu.INVENTORY:
+            if not permissions[INVENTORY]:
+                raise PermissionError
             departments_page = db.get_page(db.DEPARTMENTS,callback_data.get('page',1))
             text = 'Выберите отделение для просмотра остатков'
             reply_markup = get_inventory_department(master, departments_page, callback_data.get('page',1))
         case Menu.RECEIPTS:
+            if not permissions[RECEIPTS]:
+                raise PermissionError
             departments_page = db.get_page(db.DEPARTMENTS,callback_data.get('page',1))
             text = 'Выберите отделение для просмотра накладных'
             reply_markup = get_receipt_department(master, departments_page, callback_data.get('page',1))
         case Menu.PRODUCTS:
+            if not permissions[PRODUCTS]:
+                raise PermissionError
             products_page = db.get_page(db.PRODUCTS,callback_data.get('page',1))
             text = 'Выберите товар'
             reply_markup = get_products(master,products_page, callback_data.get('page',1))
         case Menu.CATEGORIES:
+            if not permissions[CATEGORIES]:
+                raise PermissionError
             categories_page = db.get_page(db.CATEGORIES,callback_data.get('page',1))
             text = 'Выберите категорию'
             reply_markup = get_categories(master,categories_page, callback_data.get('page',1))
         case Menu.DEPARTMENTS:
+            if not permissions[DEPARTMENTS]:
+                raise PermissionError
             departments_page = db.get_page(db.DEPARTMENTS,callback_data.get('page',1))
             text = 'Выберите отделение'
             reply_markup = kb.get_departments(master,departments_page, callback_data.get('page',1))
