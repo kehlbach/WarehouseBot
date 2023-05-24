c438f917a10745a44475980da1c810166565c007
Valentin Kelbakh <valentinkelbax@gmail.com>
2023-05-24 20:35:48 +0600

    create func generate_png

--- app/utils/tools.py
+++ app/utils/tools.py
@@ -6,6 +6,9 @@ from aiogram import types
 
 from app.data.states import Login
 
+from PIL import Image, ImageDraw, ImageFont
+import io
+import platform
 
 
 def shorten_name(name):
@@ -45,3 +48,40 @@ def has_permissions():
             return await func(*args, **kwargs)
         return wrapper
     return decorator
+
+
+def generate_png(headers, rows):
+    font_size = 20
+    if platform.system() == "Windows":
+        font = ImageFont.truetype("arial.ttf", font_size)
+    elif platform.system() == "Linux":
+        font = ImageFont.truetype("DejaVuSans.ttf", font_size)
+    cell_padding = 5  # Add some padding around the text in each cell
+    line_height = font.getsize("hg")[1] + cell_padding * 2  # Height of each row
+    header_height = font.getsize(max(headers, key=len))[1] + cell_padding * 2  # Height of header row
+    cell_widths = [max(font.getsize(str(row[i]))[0] for row in rows + [headers]) + cell_padding * 2 for i in range(len(headers))]  # Width of each column
+    image_width = sum(cell_widths)  # Width of table
+    image_height = header_height + line_height * len(rows) + cell_padding * 2  # Height of table
+    image = Image.new("RGB", (image_width, image_height), color="white")
+    draw = ImageDraw.Draw(image)
+    x = 0
+    y = 0
+    for i, header in enumerate(headers):
+        cell_width = cell_widths[i]
+        draw.rectangle((x, y, x + cell_width, y + header_height), fill=(0, 0, 0))
+        draw.text((x + cell_padding, y + cell_padding), header, font=font, fill=(255, 255, 255))
+        x += cell_width
+    x = 0
+    y += header_height
+    for row in rows:
+        for i, cell in enumerate(row):
+            cell_width = cell_widths[i]
+            draw.rectangle((x, y, x + cell_width, y + line_height), fill=(255, 255, 255), outline=(0, 0, 0))
+            draw.text((x + cell_padding, y + cell_padding), str(cell), font=font, fill=(0, 0, 0))
+            x += cell_width
+        x = 0
+        y += line_height
+    image_buffer = io.BytesIO()
+    image.save(image_buffer, format="PNG")
+    image_buffer.seek(0)
+    return image_buffer
